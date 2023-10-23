67d72c1f3c6dfb8141aa518b50e08563b28e355d
HMNIA <98679147+apon06@users.noreply.github.com>
2023-10-23 09:23:55 +0600

    improve code

--- lib/responsive_klutter.dart
+++ lib/responsive_klutter.dart
@@ -3,30 +3,30 @@ library responsive_klutter;
 import 'package:flutter/material.dart';
 
 class ResponsiveLayout extends StatelessWidget {
-  final Widget? mobileBody;
-  final int? mobileSize;
-  final Widget? tabletBody;
-  final int? tabletSize;
-  final Widget? desktopBody;
+  final Widget mobileBody;
+  final int mobileSize;
+  final Widget tabletBody;
+  final int tabletSize;
+  final Widget desktopBody;
 
   const ResponsiveLayout({
     Key? key,
-    this.mobileBody,
-    this.mobileSize,
-    this.tabletBody,
-    this.tabletSize,
-    this.desktopBody,
+    required this.mobileBody,
+    required this.mobileSize,
+    required this.tabletBody,
+    required this.tabletSize,
+    required this.desktopBody,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constactor) {
-      if (constactor.maxWidth < mobileSize!) {
-        return mobileBody!;
+      if (constactor.maxWidth < mobileSize) {
+        return mobileBody;
       }
-      if (constactor.maxWidth < tabletSize!) {
-        return tabletBody!;
+      if (constactor.maxWidth < tabletSize) {
+        return tabletBody;
       } else {
-        return desktopBody!;
+        return desktopBody;
       }
     });
   }
